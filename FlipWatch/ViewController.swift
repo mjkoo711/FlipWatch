fe6eb8ae145c2a918227ea7c97317f6f79c90914
KooMinJun <mjkoo711@gmail.com>
2019-11-17 23:41:33 +0900

    init

---
+++ FlipWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlipWatch
+//
+//  Created by 구민준 on 2019/11/17.
+//  Copyright © 2019 mjkoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
