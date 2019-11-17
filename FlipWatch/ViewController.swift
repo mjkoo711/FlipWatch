c9b6db82fdafee63430f666a48aa7521d5de25c1
KooMinJun <mjkoo711@gmail.com>
2019-11-18 01:48:21 +0900

    가로, 세로 각각 AutoLayout 설정 완료

--- FlipWatch/ViewController.swift
+++ FlipWatch/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import Splitflap
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var hour: Splitflap!
+  @IBOutlet weak var minute: Splitflap!
+  @IBOutlet weak var second: Splitflap!
+  @IBOutlet weak var adView: UIView!
+  
+  
+  // Set the text to display by animating the flaps
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    hour.datasource = self
+    minute.datasource = self
+    second.datasource = self
     // Do any additional setup after loading the view.
   }
-
-
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    hour.setText("11", animated: false)
+    minute.setText("22", animated: false)
+    second.setText("33", animated: false)
+  }
 }
 
+extension ViewController: SplitflapDataSource {
+  // Defines the number of flaps that will be used to display the text
+  func numberOfFlapsInSplitflap(_ splitflap: Splitflap) -> Int {
+    return 2
+  }
+}
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FlipWatch' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  source 'https://github.com/CocoaPods/Specs.git'
+  platform :ios, '8.0'
+
+  use_frameworks!
+  pod 'Splitflap', '~> 4.1.0'
+
+  # Pods for FlipWatch
+
+  target 'FlipWatchTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FlipWatchUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
